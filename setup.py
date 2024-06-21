95261ab51d84f97bdf835452e920f14d53d9c2a5
dianayjin <51413640+dianayjin@users.noreply.github.com>
2024-06-21 14:08:01 +0200

    initialization and loading

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We implement a ML-based trading strategy using M2, CPI, and P/E ratio data to forecast equity performance.',
+    author='diana_jin',
+    license='MIT',
+)
---
+++ src/data/make_dataset.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+import os
+
+def main(input_filepath, output_filepath):
+    """
+
+    """
+
+
+if __name__ == '__main__':
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+    main()
